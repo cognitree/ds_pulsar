apache/pulsar

[fix][admin] Fix can't delete tenant for v1 (#22550)

pulsar-broker-common/src/main/java/org/apache/pulsar/broker/resources/TopicResources.java
@@ -120,7 +120,7 @@ public class TopicResources {
         return store.exists(path)
                 .thenCompose(exists -> {
                     if (exists) {
-                        return store.delete(path, Optional.empty());
+                        return store.deleteRecursive(path);
                     } else {
                         return CompletableFuture.completedFuture(null);
                     }

cognitree/ds_pulsar

(cherry picked from commit 5d0f1d6609ee0c76177e8845300c9a20a2dc2172)

pulsar-broker-common/src/main/java/org/apache/pulsar/broker/resources/TopicResources.java
@@ -101,7 +101,7 @@ public class TopicResources {
     public CompletableFuture<Void> clearTenantPersistence(String tenant) {
         String path = MANAGED_LEDGER_PATH + "/" + tenant;
         log.info("Clearing tenant persistence for tenant: {}, path {}", tenant, path);
-        return store.deleteIfExists(path, Optional.empty());
+        return store.deleteRecursiveIfExists(path);
     }
 
     void handleNotification(Notification notification) {